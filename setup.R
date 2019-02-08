843573af1d448d14411e205e2dbad6dabfc08044
judgelord <devin.jl@gmail.com>
2019-02-08 08:25:47 -0600

    add setup script

---
+++ setup.R
@@ -0,0 +1,25 @@
+options(stringsAsFactors = F)
+
+## Add any R packages you require. 
+## Here are some we will use in 811:
+requires <- c("tidyverse", # tidyverse includes dplyr and ggplot2
+              "magrittr",
+              "foreign",
+              "readstata13",
+              "here")
+
+## Install any you don't have
+to_install <- c(requires %in% rownames(installed.packages()) == FALSE)
+install.packages(c(requires[to_install], "NA"), repos = "https://cloud.r-project.org/" )
+
+## Load all required R packages
+library(tidyverse)
+library(ggplot2); theme_set(theme_bw())
+library(magrittr)
+library(here)
+
+# Xaringan settings
+# remotes::install_github('gadenbuie/xaringanthemer')
+# remotes::install_github('yihui/xaringan')
+library("xaringan")
+library("xaringanthemer")
